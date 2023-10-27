39171bc4ce6e800b8881d13717ea703f4d5ca26f
Glory Olasanmi <gloryolasanmi@gmail.com>
2023-10-27 16:47:46 +0100

    Advancing

---
+++ func.py
@@ -0,0 +1,15 @@
+def my_function():
+    print("Success!")
+
+def function(fname, lname):
+    print(fname + " " + lname)
+
+function("Glory", "Olasanmi")
+function("Julius", "Idowu")
+function("Favour", "Olasanmi")
+
+def function1(fname, mname, lname):
+    print(fname + " " + mname + " " + lname)
+
+function1("Julius", "Oluwaropo", "Idowu")
+function1("Sholape", "Opeyemi", "Ayinde")
