95aeb5a8a76c40d4598a66fb8d949601d5b37af2
Glory Olasanmi <gloryolasanmi@gmail.com>
2023-10-28 01:00:04 +0100

    advancing

--- func.py
+++ func.py
@@ -16,21 +16,27 @@ function1("Julius", "Oluwaropo", "Idowu")
 function1("Sholape", "Opeyemi", "Ayinde")
 
 
+#Calculate the bill_amount, tip_perc
 def total_calc(bill_amount, tip_perc=10):
     total = bill_amount*(1 + 0.01*tip_perc)
     total = round(total, 2)
     print(f"Please pay ${total}")
 
+#Volume of a Cuboid
 def volume_of_cuboid(length, breadth, height):
     return length*breadth*height
-volume_of_cuboid(5, 6, 8)
+print(volume_of_cuboid(5, 6, 8))
 
+#Addition of two numbers
 def add(num1, num2):
     print("The sum of two nos: ")
     num3 = num1 +  num2
+    return (num1 + num2)
 
-add(num1=8,num2=3)
+print(add(8, 3))
+print(add(45, 12))
 
+#Odd and even no
 def evenOdd(x):
     if (x % 2 == 0):
         print("even")
@@ -45,5 +51,13 @@ def family(child1, child2, child3):
     print("It's difficult to recognize " + (child3))
 
 family(child1 = "Bola", child2 = "Ayo", child3 = "Sola")
+
+def list(food):
+    food = ["maize", "rice", "beans", "yam", "noodles", "bread"]
+
+lst = ['sugar', 'milk', 'cream', 'honey', 'butter podding']
+list(lst)
+print(lst)
+
 
 #Python Lambda
